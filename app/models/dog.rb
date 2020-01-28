7c0a849d8ce4a7f90d592e487efbdf6422a8fbc8
github username <github email address>
2020-01-27 20:25:08 -0500

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,20 @@
+# require_relative '../config/environment'
+
+class Dog 
+    attr_accessor :name, :breed, :age
+
+    DOGS = []
+
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+
+        DOGS << self 
+    end
+
+    def self.all
+        DOGS 
+    end 
+
+end 
